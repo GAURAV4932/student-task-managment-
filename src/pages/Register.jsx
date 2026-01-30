7f88b851cd9d8cfff31472e757bd40ad815f29c3
gaurav <manubhaikindarkhediya1@gmail.com>
2026-01-30 13:48:43 +0530

    [gk][new added]

---
+++ src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <>
+      <div className="from-container">
+        <h1>This is register page.</h1>
+      </div>
+    </>
+  );
+};
+
+export default Register;
