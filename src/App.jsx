f3db35e5850c2e364c4f001f8194d3421ab1c850
gaurav <manubhaikindarkhediya1@gmail.com>
2026-01-31 13:01:23 +0530

    [gk][new added]

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header.jsx'
-import Register from './pages/Register.jsx'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
 function App() {
-  const [count, setCount] = useState(0)
+  const route = createBrowserRouter([
+    {
+      path: "/Login",
+      element: <Login />,
+    },
+    {
+      path: "/Register",
+      element: <Register />,
+    },
+  ]);
 
-  return (
-    <>
-      <Header /> 
-      <Register />
-    </>
-  )
+  return <RouterProvider router={route} />;
 }
-
-export default App
+export default App;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,52 @@
-import React from "react";
-
+import "./Register.css";
 const Register = () => {
   return (
     <>
       <div className="from-container">
-        <h1>This is register page.</h1>
+        <h1 className="from-titl">Register</h1>
+        <from>
+          <div className="from-group">
+            <label htmlFor="name">Full Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Enter you full name"
+            />
+          </div>
+          <div className="from-group">
+            <label htmlFor="email">email Address</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter you email"
+            />
+          </div>
+          <div className="from-group">
+            <label htmlFor="phone">Phone Number</label>
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              placeholder="Enter you phone number"
+            />
+          </div>
+          <div className="from-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="create a password"
+            />
+          </div>
+          <button type="submit" className="btn-primary">
+            Register
+          </button>
+
+          <p className="link-text"/>
+        </from>
       </div>
     </>
   );
